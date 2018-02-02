0dad126fda097d6a622500fb8437ff2f54c33bd4
7CT <fizza.azam@geniteam.com>
2018-02-02 19:24:36 +0500

    kkkkkl

--- app/src/main/java/com/geniteam/firebasesignup/UserProfile.java
+++ app/src/main/java/com/geniteam/firebasesignup/UserProfile.java
@@ -13,6 +13,8 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,6 +37,7 @@ public class UserProfile extends AppCompatActivity {
             if(getIntent().hasExtra(FireBaseConstants.FIREBASE_USER_ID_Key)){
                 userID=getIntent().getStringExtra(FireBaseConstants.FIREBASE_USER_ID_Key);
             binding.progressBarsignup.setVisibility(View.VISIBLE);
+             /**
                 databaseReferenceUser.addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
@@ -78,6 +81,28 @@ public class UserProfile extends AppCompatActivity {
                     }
 
                 });
+**/
+
+
+                // more accurat
+                databaseReferenceUser.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        User user=dataSnapshot.getValue(User.class);
+
+                        Log.d("debug","name "+user.name);
+                        Log.d("debug","email "+user.email);
+                        Log.d("debug","password "+user.pass);
+                        Log.d("debug","url "+user.photoUrl);
+                        setUpUi(user);
+                        Toast.makeText(getApplicationContext(),user.name+" Login successfully ",Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
             }
         }
     }
@@ -85,10 +110,29 @@ public class UserProfile extends AppCompatActivity {
 
 
     public void setUpUi(User user){
+        try{
+
+        }catch (Exception e){
+
+        }
         binding.userName.setText(user.name);
         binding.emailTv.setText(user.email);
         binding.passwordTv.setText(user.pass);
         binding.address.setText(user.Address);
         binding.progressBarsignup.setVisibility(View.GONE);
+        binding.progressBarImage.setVisibility(View.VISIBLE);
+        Picasso.with(this).load(user.photoUrl).into(binding.imageViewProfile, new Callback() {
+            @Override
+            public void onSuccess() {
+                binding.progressBarImage.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onError() {
+                binding.progressBarImage.setVisibility(View.GONE);
+
+            }
+        });
+
     }
 }
